e42bde988a42273b0a41e6bda3f3e2292242bf8b
JunhaKwon2000 <junharoket.naver.com>
2025-04-08 22:17:10 +0900

    4월8일 자기 전 커밋
    구글 검색창 흉내내기 진행
    (*02_test 경로에 있습니다*)

--- 02_test/js/main.js
+++ 02_test/js/main.js
@@ -1,1 +1,8 @@
 console.log('외부 스크립트'); // 콘솔창에 log(기록)를 남기겠다
+
+const icon = document.querySelector('.searchIcon');
+const input = document.getElementById('search');
+
+icon.addEventListener('click', () => {
+    input.focus(); // input에 커서 넣기!
+});
