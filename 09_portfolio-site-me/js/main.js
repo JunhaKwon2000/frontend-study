932688f9fb4d94e2d07599cf5e76c6b07140bc78
JunhaKwon2000 <junharoket@naver.com>
2025-04-21 22:51:23 +0900

    js 폴더 업데이트
    개인 포트폴리오 사이트 업데이트

--- 09_portfolio-site-me/js/main.js
+++ 09_portfolio-site-me/js/main.js
@@ -1,12 +1,10 @@
-let i = 0;
-const txt = '테스트테스트테스트';
-const speed = 180;
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.querySelector('.intro').innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
-typeWriter();
+const projectDiv = document.querySelector('.visual > .inner div:nth-child(2)');
+const projectDivButton = document.querySelector('.visual > .inner div:nth-child(2) a');
+projectDivButton.addEventListener('mouseover', () => {
+  projectDiv.classList.add('animate__animated');
+  projectDiv.classList.add('animate__heartBeat');
+})
+projectDivButton.addEventListener('mouseout', () => {
+  projectDiv.classList.remove('animate__animated');
+  projectDiv.classList.remove('animate__heartBeat');
+})
